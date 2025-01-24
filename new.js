9166327b4e9c1a23ff91eb1c75efe328009b414d
dakshadha <dakshadha2005@gmail,com>
2025-01-24 14:48:39 +0530

    msg

---
+++ new.js
@@ -0,0 +1,12 @@
+let user={
+    name:"Sam",
+    age:30,
+    "is Singer":true,
+}
+for(let key in user){
+    console.log(key+":"+user[key]);
+}
+let array=[1,2,3,4,5,6]
+for(let value of array){
+    console.log(value);
+}
---
+++ script.js
@@ -0,0 +1,22 @@
+let a=10
+let b=20
+console.log(a+b);
+function add(x,y) {
+    return x+y;
+}
+const add =(x,y)=>{
+    return x+y;
+}
+function handleLogin(){
+    const username=document.getElementById('user').value;
+    const password=document.getElementById('pass').value;
+    const error=document.getElementById('error')
+
+    const loginData=[
+        {username:"charles",password:"charles16"},
+        {username:"carlos",password:"carlos55"},
+        {username:"username",password:"password"},
+    ]
+
+    const isValid= loginData.some((user)=>{user.username===username && user.password===password})
+}
